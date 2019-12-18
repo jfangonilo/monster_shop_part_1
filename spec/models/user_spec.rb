fb6364765e13958e99b609a79592e40a7d885656
Raymond Nguyen <rayknguyen@gmail.com>
2019-12-18 16:35:18 -0700

    Add association test for user order for user spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -10,6 +10,9 @@ RSpec.describe User, type: :model do
     it {should validate_presence_of(:email)}
     it {should validate_uniqueness_of(:email)}
     it {should validate_presence_of(:password)}
+
+    it { should have_many :user_orders}
+    it { should have_many(:orders).through(:user_orders)}
   end
 
   describe "roles" do
