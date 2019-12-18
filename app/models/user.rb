854fe800f4df54b93ee8b07b3d6b0b5624fabad0
Raymond Nguyen <rayknguyen@gmail.com>
2019-12-18 16:37:22 -0700

    Add association for user for user order

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,9 @@ class User <ApplicationRecord
   validates_uniqueness_of :email
   validates_confirmation_of :password
 
+  has_many :user_orders
+  has_many :orders, through: :user_orders
+
   has_secure_password
 
   enum role: %w(user merchant_employee merchant_admin admin_user)
