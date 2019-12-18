cfeaf35c752f9851e12f3978fe1b5264eb6329f4
Raymond Nguyen <rayknguyen@gmail.com>
2019-12-18 16:38:32 -0700

    Add association for order for user order

--- app/models/order.rb
+++ app/models/order.rb
@@ -4,6 +4,10 @@ class Order <ApplicationRecord
   has_many :item_orders
   has_many :items, through: :item_orders
 
+  has_many :user_orders
+  has_many :users, through: :user_orders
+  
+
   def grandtotal
     item_orders.sum('price * quantity')
   end
